fc769c4635f95d22a7dfdd9854846feb5d85e8c6
hschmidtchen <hagen.schmidtchen@gmail.com>
2014-05-12 16:45:12 +0200

    added the altera Hello_World TestProjekt(NiosII)

---
+++ TestProjekt/db/myfirst_niosii.sas
@@ -0,0 +1,1 @@
+myfirst_niosii/DONE
---
+++ TestProjekt/sopc_add_qip_file.tcl
@@ -0,0 +1,3 @@
+project_open -current_revision "C:/Users/HS/Desktop/TestProjekt/myfirst_niosii.qpf"
+set_global_assignment -name QIP_FILE "DE2_115_SOPC.qip"
+project_close
